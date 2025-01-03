794d916ba54be64567407da078ee339f253c475b
Tarun sharma <tarunsharmawee@gmail.com>
2025-01-03 13:01:42 +0530

    add database

--- app/api/v1/signup/route.ts
+++ app/api/v1/signup/route.ts
@@ -1,10 +1,26 @@
 import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+import prismaClient from "../../../lib/db"
 
-export async function POST(req : NextRequest){
+export async function POST(req: NextRequest) {
+  const data = await req.json();    
 
-    const data = await req.json()
+  prismaClient.user.create({
+    data: {
+      username: data.username,
+      password: data.password,
+    },
+  });
+  return NextResponse.json({
+    message : "you have signed up"
+  })
+}
+
+export async function GET(req : NextRequest,){
+    const user = await prismaClient.user.findFirst();
 
     return NextResponse.json({
-        message: "you have been signed up"
+        user,
     })
 }
+
